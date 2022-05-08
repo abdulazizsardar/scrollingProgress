43b7d3339e679c6acac360df26578e47b71ba6ce
abdulaziz <67181790+abdulazizsardar@users.noreply.github.com>
2022-05-08 12:46:38 +0600

    Add files via upload

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# scrolling progress bar with tailwindcss & alpinejs
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/A_kamel/pen/qBmmGKJ](https://codepen.io/A_kamel/pen/qBmmGKJ).
+
+scrolling progress bar with tailwindcss & alpinejs
---
+++ script.js
@@ -0,0 +1,13 @@
+const scrollProgress = () => {
+  return {
+    init() {
+      window.addEventListener('scroll', () => {
+        let winScroll = document.body.scrollTop || document.documentElement.scrollTop
+        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        this.percent = Math.round((winScroll / height) * 100)
+      })
+    },
+    circumference: 30 * 2 * Math.PI,
+    percent: 0,
+  }
+}
